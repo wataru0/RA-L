0e1280796a206ec672c6237d53e4a5217a58d9b9
wataru0 <wataru.tv.nanao@gmail.com>
2021-04-19 13:53:53 +0900

    ロボットの動作確認用プログラムの作成

---
+++ demo2.py
@@ -0,0 +1,20 @@
+import os
+import gym
+import gym_custom
+
+env = gym.make('CustomAnt-v0')
+env.reset()
+done = False
+
+nd_dir = "./tmp/"
+os.makedirs(nd_dir, exist_ok=True)
+
+while True:
+    env.render()
+    if done:
+        break
+    action = env.action_space.sample()
+    obs, reward, done, info = env.step(action)
+
+    # print(info["a"])
+    # print(info['reward_forward'])
