239e7869a433284b6b4710b97ee77357d06fb032
wataru0 <wataru.tv.nanao@gmail.com>
2021-05-12 11:17:48 +0900

    modify

--- eval_generalization_customEnv.sh
+++ eval_generalization_customEnv.sh
@@ -12,7 +12,14 @@
 # python eval_generalization_customEnv.py --agent=Curriculum-v4-16million --n_episodes=10
 # python eval_generalization_customEnv.py --agent=Curriculum2-v1 --n_episodes=10
 
-python eval_generalization_customEnv.py --agent=Baseline --n_episodes=10
-python eval_generalization_customEnv.py --agent=UDR --n_episodes=10
-python eval_generalization_customEnv.py --agent=CDR-v1 --n_episodes=10
-python eval_generalization_customEnv.py --agent=CDR-v2 --n_episodes=10
+# 新しくトレーニングしたもの（従来通りの普通の終了条件）
+# python eval_generalization_customEnv.py --agent=Baseline --n_episodes=10
+# python eval_generalization_customEnv.py --agent=UDR --n_episodes=10
+# python eval_generalization_customEnv.py --agent=CDR-v1 --n_episodes=10
+# python eval_generalization_customEnv.py --agent=CDR-v2 --n_episodes=10
+
+# 新しくトレーニングしたもの（従来の終了条件に転倒したら終了するを追加して訓練したもの）
+python eval_generalization_customEnv.py --agent=Baseline_CustomAnt --n_episodes=10
+python eval_generalization_customEnv.py --agent=UDR_CustomAnt --n_episodes=10
+python eval_generalization_customEnv.py --agent=CDR-v1_CustomAnt --n_episodes=10
+python eval_generalization_customEnv.py --agent=CDR-v2_CustomAnt --n_episodes=10
