cb06384251e60a60b990b194003abea53d535552
wataru0 <wataru.tv.nanao@gmail.com>
2021-06-15 02:31:05 +0900

    ランダムに関節を2個壊す環境での評価の追加

--- eval_generalization_customEnv.sh
+++ eval_generalization_customEnv.sh
@@ -34,7 +34,7 @@
 # python eval_generalization_customEnv.py --agent=CDR-v2_CustomAnt-ReduceSRto0IfFallingDown_bf100 --n_episodes=10
 # python eval_generalization_customEnv.py --agent=CDR-v1_CustomAnt-ReduceSRto0IfFallingDown_upperfix_bf100 --n_episodes=10
 # python eval_generalization_customEnv.py --agent=CDR-v2_CustomAnt-ReduceSRto0IfFallingDown_lowerfix_bf100 --n_episodes=10
-# python eval_generalization_customEnv.py --agent=Baseline_CustomAnt-ReduceSRto0IfFallingDown-v2 --n_episodes=10
+python eval_generalization_customEnv.py --agent=Baseline_CustomAnt-ReduceSRto0IfFallingDown-v2 --n_episodes=10
 python eval_generalization_customEnv.py --agent=UDR_CustomAnt-ReduceSRto0IfFallingDown_k0015 --n_episodes=10
 python eval_generalization_customEnv.py --agent=CDR-v1_CustomAnt-ReduceSRto0IfFallingDown_bf100_k0015 --n_episodes=10
 python eval_generalization_customEnv.py --agent=CDR-v2_CustomAnt-ReduceSRto0IfFallingDown_bf100_k0015 --n_episodes=10
