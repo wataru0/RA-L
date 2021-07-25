575f9bd972a3a76ba49b4395f9187376e0657053
wataru0 <wataru.tv.nanao@gmail.com>
2021-07-25 22:12:14 +0900

    add

--- plot_bar.sh
+++ plot_bar.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-python plot_bar.py --agent=Baseline_Ant-v2
-python plot_bar.py --agent=Baseline_Ant-v2_k08
-python plot_bar.py --agent=Baseline_Ant-v2_k06
-python plot_bar.py --agent=Baseline_Ant-v2_k04
-python plot_bar.py --agent=Baseline_Ant-v2_k02
-python plot_bar.py --agent=Baseline_Ant-v2_k00
+# python plot_bar.py --agent=Baseline_Ant-v2
+# python plot_bar.py --agent=Baseline_Ant-v2_k08
+# python plot_bar.py --agent=Baseline_Ant-v2_k06
+# python plot_bar.py --agent=Baseline_Ant-v2_k04
+# python plot_bar.py --agent=Baseline_Ant-v2_k02
+# python plot_bar.py --agent=Baseline_Ant-v2_k00
+python plot_bar.py --agent=Baseline_Ant-v2_k12
