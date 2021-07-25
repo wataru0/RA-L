3666454564b78250e7c604b6ecc8a77f1d0c8a30
wataru0 <wataru.tv.nanao@gmail.com>
2021-07-25 22:12:00 +0900

    add

--- eval_for_bar.sh
+++ eval_for_bar.sh
@@ -10,9 +10,10 @@
 # python eval_for_bar.py --agent=LCL-v1_CustomAnt-ReduceSRto0IfFallingDown_k0015 --n_episodes=50
 # python eval_for_bar.py --agent=LCL-v2_CustomAnt-ReduceSRto0IfFallingDown_k0015 --n_episodes=50
 
-python eval_for_bar.py --agent=Baseline_Ant-v2 --n_episodes=20
-python eval_for_bar.py --agent=Baseline_Ant-v2_k08 --n_episodes=20
-python eval_for_bar.py --agent=Baseline_Ant-v2_k06 --n_episodes=20
-python eval_for_bar.py --agent=Baseline_Ant-v2_k04 --n_episodes=20
-python eval_for_bar.py --agent=Baseline_Ant-v2_k02 --n_episodes=20
-python eval_for_bar.py --agent=Baseline_Ant-v2_k00 --n_episodes=20
+# python eval_for_bar.py --agent=Baseline_Ant-v2 --n_episodes=20
+# python eval_for_bar.py --agent=Baseline_Ant-v2_k08 --n_episodes=20
+# python eval_for_bar.py --agent=Baseline_Ant-v2_k06 --n_episodes=20
+# python eval_for_bar.py --agent=Baseline_Ant-v2_k04 --n_episodes=20
+# python eval_for_bar.py --agent=Baseline_Ant-v2_k02 --n_episodes=20
+# python eval_for_bar.py --agent=Baseline_Ant-v2_k00 --n_episodes=20
+python eval_for_bar.py --agent=Baseline_Ant-v2_k12 --n_episodes=20
