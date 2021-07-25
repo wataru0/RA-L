986da1ab99a2160b7ae10dd38fb940f41e7ae673
wataru0 <wataru.tv.nanao@gmail.com>
2021-07-26 02:28:23 +0900

    add

--- plot_bar.sh
+++ plot_bar.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-# python plot_bar.py --agent=Baseline_Ant-v2
-# python plot_bar.py --agent=Baseline_Ant-v2_k08
-# python plot_bar.py --agent=Baseline_Ant-v2_k06
-# python plot_bar.py --agent=Baseline_Ant-v2_k04
-# python plot_bar.py --agent=Baseline_Ant-v2_k02
-# python plot_bar.py --agent=Baseline_Ant-v2_k00
+# 平均報酬の出力
+python plot_bar.py --agent=Baseline_Ant-v2
+python plot_bar.py --agent=Baseline_Ant-v2_k08
+python plot_bar.py --agent=Baseline_Ant-v2_k06
+python plot_bar.py --agent=Baseline_Ant-v2_k04
+python plot_bar.py --agent=Baseline_Ant-v2_k02
+python plot_bar.py --agent=Baseline_Ant-v2_k00
 python plot_bar.py --agent=Baseline_Ant-v2_k12
+
+# 平均進捗の出力
+python plot_bar.py --agent=Baseline_Ant-v2 --plotType=forward
+python plot_bar.py --agent=Baseline_Ant-v2_k08 --plotType=forward
+python plot_bar.py --agent=Baseline_Ant-v2_k06 --plotType=forward
+python plot_bar.py --agent=Baseline_Ant-v2_k04 --plotType=forward
+python plot_bar.py --agent=Baseline_Ant-v2_k02 --plotType=forward
+python plot_bar.py --agent=Baseline_Ant-v2_k00 --plotType=forward
+python plot_bar.py --agent=Baseline_Ant-v2_k12 --plotType=forward
